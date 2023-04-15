15a8925e730762e4e2c89e7c2219dd7741a29a81
cityrobo <84413175+cityrobo@users.noreply.github.com>
2023-04-15 19:00:20 +0200

    Added a lists for heating effects, because putting all effects on the same game object might not always be the best idea.

--- ModularWorkshop/HeatingEffectAddon.cs
+++ ModularWorkshop/HeatingEffectAddon.cs
@@ -7,13 +7,20 @@ using OpenScripts2;
 namespace ModularWorkshop
 {
     [RequireComponent(typeof(ModularWeaponPart))]
-    [RequireComponent(typeof(FirearmHeatingEffect))]
     public class HeatingEffectAddon : MonoBehaviour , IPartFireArmRequirement
     {
+        [Header("This list is optional, you can also place the Heating effects scripts on this gameobject instead. But please not both!")]
+        public FirearmHeatingEffect[] FirearmHeatingEffects;
         public FVRFireArm FireArm
         { 
             set 
             {
+                foreach (var HeatingEffect in FirearmHeatingEffects)
+                {
+                    HeatingEffect.FireArm = value;
+                    HeatingEffect.Reload();
+                }
+
                 foreach (var HeatingEffect in GetComponents<FirearmHeatingEffect>())
                 {
                     HeatingEffect.FireArm = value;
