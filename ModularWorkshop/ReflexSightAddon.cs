1206192d512cfc7ebf0f508f6c540e5264b7f171
cityrobo <84413175+cityrobo@users.noreply.github.com>
2023-04-16 14:34:53 +0200

    integrated reflex sight support

---
+++ ModularWorkshop/ReflexSightAddon.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using FistVR;
+using OpenScripts2;
+
+namespace ModularWorkshop
+{
+    [RequireComponent(typeof(ModularWeaponPart))]
+    public class ReflexSightAddon : MonoBehaviour , IPartFireArmRequirement
+    {
+        public CustomReflexSightInterface ReflexSightInterface;
+
+        public FVRFireArm FireArm
+        { 
+            set 
+            {
+                ReflexSightInterface.FireArm = value;
+                ReflexSightInterface.IsIntegrated = true;
+                ReflexSightInterface.ForceInteractable = true;
+                ReflexSightInterface.OffsetReticle();
+            } 
+        }
+    }
+}
